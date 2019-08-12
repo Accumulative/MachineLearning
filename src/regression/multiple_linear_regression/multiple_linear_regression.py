3b6b1e4291fe4be6b7981b50a4d20117800f936f
Kieran Burke <kieran314237@hotmail.co.uk>
2019-08-12 14:29:02 +0900

    [add] multiple linear regression api controller [add] one hot encoding and the dummy variable trap explanation

---
+++ src/regression/multiple_linear_regression/multiple_linear_regression.py
@@ -0,0 +1,21 @@
+# Multiple Linear Regression
+
+# Importing the libraries
+from src.common import main, visualize
+from sklearn.linear_model import LinearRegression
+import os
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, '50_Startups.csv')
+
+def execute():
+    X_train, X_test, y_train, y_test = main.import_dataset(filename, 0.2, 3)
+
+    # Fitting Simple Linear Regression to the Training set
+    regressor = LinearRegression()
+    regressor.fit(X_train, y_train)
+
+    # Predicting the Test set results
+    # y_pred = regressor.predict(X_test)
+    predictions = regressor.predict(X_train)
+    return visualize.plot(X_train, y_train, X_test, y_test, predictions)
+
